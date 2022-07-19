939b4299c3da8df0ab93cf7cb0761ebb046f8a66
scm-minaungpaing <min.aungpaing@seattleconsultingmyanmar.com>
2022-07-19 22:28:11 +0630

    add socketIo

--- server/server.js
+++ server/server.js
@@ -1,13 +1,26 @@
 const path = require('path')
 const express = require('express')
+const socketIO = require('socket.io')
+const http = require('http')
 
 const publicPath = path.join(__dirname,'../public')
 const port = process.env.PORT || 3000
 const app = express();
+let server = http.createServer(app)
+let io = socketIO(server)
 
 app.use(express.static(publicPath))
 
-app.listen( port, () => {
+io.on('connection', (socket) => {
+    console.log('A new user just connected')
+
+    socket.on('disconnect', () => {
+        console.log('User was Disconnected.')
+    })
+})
+
+
+server.listen( port, () => {
     console.log(`Server is running on port ${port}`)
 })
 
